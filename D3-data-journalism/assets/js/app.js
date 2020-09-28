fc903a851910a3cf0d956c7d7d53e80388bd1ee4
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-09-28 16:49:36 +1000

    Create app.js

---
+++ D3-data-journalism/assets/js/app.js
@@ -0,0 +1,56 @@
+// STEP ONE
+// Create and format the SVG
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+  };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(hairData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8,
+        d3.max(hairData, d => d[chosenXAxis]) * 1.2
+      ])
+      .range([0, width]);
+  
+    return xLinearScale;
+  
+  }
+
+// Use D3 to read in samples.json
+var pathSamples = `assets/data/data.csv`;
+var dataDB;
+
+// Fetch json file using D3 to test in local machine
+d3.csv(pathSamples)
+    .then(data => {dataDB = data
+
+    console.log(dataDB);
+    console.log(dataDB[0].healthcare);
+
+});
